d4c8b41e1e5f4836ade310c38fdcfa313b6f85d6
nxtstep101 <nxtstep101@gmail.com>
2017-10-18 12:47:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,27 +3,23 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <fstream>
-#include <time.h>
+#include <ctime>
 using namespace std; 
 
 int main() {
-	// define variables 
 	char userPlay;
 	int userSpace = 0;
 	int dieRoll = 0;
 	int numPlayers = 0;
-	
-	// arrays
-	//int money[];
+	int goodOrBad = 0;
+	int moneyAmt = 0;
+	int mostMoney = 0;
+	int p = 0;
+	ifstream inFS; // input file stream
 
-	// SLIDE 6.4 FOR ALGORITHM FOR FINDING MAXIMUM VALUE
-	
-	// SLIDE 6.6 FOR SETTING ALL ARRAY ELEMENTS TO 0 (FOR RESTARTING GAME)
-	
-	// welcome to my game
 	cout << "WELCOME TO MOKELAND!" << endl; 
 
-	//while (userPlay != 'n') {
+	while (userPlay != 'n') {
 		cout << "How many users will be playing: ";
 		cin >> numPlayers;
 		while (numPlayers > 20 || numPlayers < 1) {		// makes sure user number is between
@@ -33,23 +29,24 @@ int main() {
 		}
 		
 		string names[numPlayers]; 	// names array
+		int boardSpace[numPlayers];	// boards array
+		int money[numPlayers];		// money array
 		cin.ignore(); 	// prevents the getline below from getting messed up
 		
 		for (int x = 0; x < numPlayers; x++) {
 			cout << "Enter name of player " << x + 1 << ": ";	// loop to have you input all of the players names
 			getline(cin, names[x]);
+			boardSpace[x] = 0;	// initializes all subscripts of boardSpace to zero
+			money[x] = 0;	// intializes all subscripts of money to zero
 		}
 		
-		int boardSpace[numPlayers]; // creates a seperate subscript for each user to keep track of their place on the board
-
 		do {
-			int p = 0;
 			for (p = 0; p < numPlayers; p++) {
 				cout << names[p] << " press enter to roll die!" << endl;
 				if (cin.get() == '\n') {	// waits for user to hit enter
-					srand(time(NULL));		// changes the seed everytime you play
+					srand(time(NULL));		// changes the seed everytime you roll
 					dieRoll = (rand() % 6) + 1;
-					cout << "You rolled a " << dieRoll << "!" << endl << endl;
+					cout << "You rolled a " << dieRoll << "!" << endl;
 					switch(dieRoll) {
 						case 1:
 							cout << " _________\n";
@@ -94,12 +91,36 @@ int main() {
 							cout << "|_________|\n\n";
 							break;
 					}
-					boardSpace[p] = boardSpace[p + dieRoll];
-					cout << boardSpace[p] << endl;
+					boardSpace[p] = dieRoll + boardSpace[p];
+					cout << names[p] << ": " << boardSpace[p] << endl; // ONLY FOR TESTING remove later!
+					goodOrBad = (rand() % 2) + 1;
+					
+					if (goodOrBad == 1) { // good
+						cout << ":-D" << endl;
+						moneyAmt = (rand() % 100000) + 1;
+						money[p] = money[p] + moneyAmt;
+						cout << "You have: $" << money[p] << endl;
+					}
+					else { // bad
+						cout << ":-(" << endl;
+						moneyAmt = (rand() % 100000) + 1;
+						money[p] = money[p] - moneyAmt;
+						cout << "You have: $" << moneyAmt << endl;
+					}
 				}
-			} while (boardSpace[p] < 25);
+			}
+		} while (boardSpace[p] < 25);
+		
+		mostMoney = money[0];	
+		
+		for (int i = 0; i < numPlayers; i++) {
+			if (money[i] > mostMoney) {
+				money[i] = mostMoney;
+			}
 		}
-	//}
-	
+		cout << mostMoney << endl;
+		cout << "Do you want to play again? y/n" << endl;
+		cin >> userPlay;
+	}
 	return 0;
 }
